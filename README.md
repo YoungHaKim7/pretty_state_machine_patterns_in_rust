49bace4e984f05081cd0fbe94ffa113a0d56cb1d
GYoung <ytok1108@aol.com>
2025-09-24 21:16:13 +0900

    https://hoverbear.org/blog/rust-state-machine-pattern/

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pretty_state_machine_patterns_in_rust
 https://hoverbear.org/blog/rust-state-machine-pattern/
+
+# Result
+
+```bash
+my_machine = Machine
+Initial state: Waiting { waiting_time: 0ns }
+After transition: Filling { rate: 1 }
+
+```
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,48 @@
 #[derive(Debug)]
 struct Machine;
+
+#[derive(Debug)]
 struct BottleFiller {
-    state : BottleFillerState
+    state: BottleFillerState
 }
 
-struct State {
-    Waiting : { waiting_time: std::time::Duration}, 
-    Filling :{rate: usize}, 
+#[derive(Debug)]
+enum BottleFillerState {
+    Waiting { waiting_time: std::time::Duration },
+    Filling { rate: usize },
     Done
 }
 
-struct StateMachine {state: State}
+#[derive(Debug)]
+struct StateMachine {
+    state: BottleFillerState
+}
 
 impl StateMachine {
     fn new() -> Self {
         StateMachine {
-            state: State::Waiting { waiting_time: std::time::Duration::new(0,0)}
+            state: BottleFillerState::Waiting { 
+                waiting_time: std::time::Duration::new(0, 0) 
+            }
         }
     }
 
     fn to_filling(&mut self) {
-        self.state = match self.state {
-            State::Waiting {} => State::Filling {rate:1},
+        self.state = match &self.state {
+            BottleFillerState::Waiting { .. } => BottleFillerState::Filling { rate: 1 },
             _ => panic!("Invalid state transition!"),
         }
     }
 }
 
-enum BottleFillerState {
-    Waiting = {wating_time: std::time::Duration},
-    Filling{rate:usize},
-    Done
-}
-
 
 fn main() {
     let my_machine = Machine;
-    let mut sate_machine = StateMachine::new();
+    let mut state_machine = StateMachine::new();
 
     println!("my_machine = {my_machine:?}");
+    println!("Initial state: {:?}", state_machine.state);
+    
+    state_machine.to_filling();
+    println!("After transition: {:?}", state_machine.state);
 }
